597591cc5ce4ed6042f48dd388155af9293c0c2e
Nicu <nikusor211007@gmail.com>
2025-03-06 20:57:59 +0200

    am creat fetch

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Happy developing ✨')
+const requestURL = "https://www.googleapis.com/books/v1/volumes?q=harry+potter"
+
+
+const request = async() =>{
+    const response = await fetch(requestURL)
+    try {
+        if(!response.ok){
+            throw new Error('Error fetching data')
+        }
+    } catch (error) {
+        const convertionJson = await response.json()
+        const convertor = convertionJson
+
+        convertor.forEach(element => {
+            console.log(element);
+            
+        });
+    }
+}
